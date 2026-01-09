acd9e2c542643b9000023a5b21e87ab0003a478b
bujipenguin <bryanbuji@gmail.com>
2026-01-09 14:59:28 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+import pyautogui #using this instead of win32api for general support, also since im on mac
+
+#variables
+
+def start():
+    return False
+
+#TODO figure out logic for button presses and detection
+#TODO collect images/colors for open cv to detect
+#TODO figure out how the hell pyautogui even works
